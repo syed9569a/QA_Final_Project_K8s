515975060ffaaa42e1f1d320a88bc6b1351c0a98
Ubuntu <ubuntu@ip-172-31-9-49.eu-west-1.compute.internal>
2020-09-08 19:26:21 +0000

    dekaying nginx start

--- scripts/kubernetes_deploy.sh
+++ scripts/kubernetes_deploy.sh
@@ -27,5 +27,8 @@ kubectl apply -f ./pods/backend.yml
 output "Deploying the frontend object"
 kubectl apply -f ./pods/frontend.yml
 
+output "Delay NGINX start"
+sleep 5m
+
 output "Deploying the nginx object"
 kubectl apply -f ./pods/nginx.yml
